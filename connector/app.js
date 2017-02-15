63d5204b577e86af19304cc452848a5d370db8c3
Weipeng cao <wwcao@colorado.edu>
2017-02-15 15:37:14 -0700

    skeloton of connector

---
+++ connector/app.js
@@ -0,0 +1,68 @@
+var app = require('./config/app.json');
+app.serverConf = require('./config/server.json');
+var sleeptime = 1;
+var failCounter = 0;
+const maxsleep = 16;
+const maxfailed = 10;
+console.log(app);
+
+app.senduuid = function(){
+    var request = require('request');
+    const port = app.serverConf.port;
+    const ip = app.serverConf.ip;
+    const protocol = app.serverConf.httpp;
+    const base = protocol+"://"+ip+":"+port;
+    const req_url = '/rest';
+    console.log(base);
+    var body = app.hardware;
+    console.log(body);
+    return true;
+    const options = {
+        url: base+req_url,
+        method: 'GET',
+        body:body
+    };
+    request(options, function(err, res, body) {
+        console.log(body) 
+        if(!err && res.statusCode == 200) {
+            console.log(body);
+            return true;
+        }
+        else {
+            //return false;
+            return true;
+        }
+    });
+};
+
+app.identityHub = function() {
+    var i=0;
+    while(!app.senduuid()) {
+        setTimeout(function(){
+            console.log((i++)+" Failed to identify hub");
+        }, 60000);
+
+        sleeptime += 2;
+        if(sleeptime > 180) {
+            sleeptime = 180;
+        }
+        console.log((i++) +"sleep time: " + sleeptime);
+
+    }
+    sleeptime = 1;
+    return true;
+};
+
+app.runRountines = function() {
+    return
+};
+
+app.run = function() {
+    console.log("Running");
+    app.identityHub();
+    app.runRoutines();
+    console.log("Unexpected termination");
+};
+
+app.run();
+
