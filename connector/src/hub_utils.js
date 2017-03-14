9efc8c49b7c08f4f272183b0f3ea951a5cace9d0
wwcao <wwcao@colorado.edu>
2017-03-13 22:43:02 -0600

    minor fixed

--- connector/src/hub_utils.js
+++ connector/src/hub_utils.js
@@ -1,11 +1,11 @@
 module.exports = {
   getUrl:function(json) {
-    return 'http://localhost:8080'+"/rest/items/";
+    return 'http://localhost:8080'+"/rest/items";
   },
   getOptions: function(json) {     // construct request options
     var url = this.getUrl();
     if(json.itemname != '')
-      url += json.itemname;
+      url += '/'+json.itemname;
     var options = {
       method:json.method,
       uri: url,
